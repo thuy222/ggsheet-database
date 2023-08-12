620f24b0b3d2fc4f50c8910da81456c54bf80fb9
thuy.ho <thuy.ho@sunteco.io>
2023-08-12 10:53:09 +0700

    gg sheet database

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
+
+const url =
+  "https://sheet.best/api/sheets/3e416eae-461d-48a1-ae11-e3e1f52f9ee5";
+
+const apikey =
+  "7uI3FXmM4NxxPT91t%v8J%wIL$Q20p7yJvKFu1HRL$6AV-6UT3zSAn1A5QiS%913";
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  const info = {
+    name: name,
+    age: age,
+  };
+  const submitHandler = async (e) => {
+    console.log(info);
+    e.preventDefault();
+
+    axios
+      .post(url, info, { headers: { "x-api-key": apikey } })
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>
+          Enter your name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
+        </label>
+        <button type="submit" onClick={submitHandler}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
